b8db9ab9aae4e0a1d5cdc745641e5250c4f82f3f
Joel Razuri <jrazuri98@hotmail.com>
2025-04-07 20:34:13 -0300

    importanto clases

---
+++ main.py
@@ -0,0 +1,10 @@
+from .classes.book import Book
+from .classes.librarian import Librarian
+from .classes.library import Library
+from .classes.loan import Loan
+from .classes.user import User
+
+
+library = Library('Biblioteca del Pueblo')
+
+
